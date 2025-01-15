3f767fa1519830952b9f37dd553f560fed35df66
RickFDND <rick.pijnacker@hva.nl>
2025-01-15 17:13:37 +0100

    img slider netjes en deel van hamburger menu gemaakt

---
+++ main.js
@@ -0,0 +1,7 @@
+let hamburgerMenu = document.querySelector('.hamburger');
+let navbar = document.querySelector('nav');
+hamburgerMenu.addEventListener('click', function(){
+    navbar.classList.toggle('hamburger-menu-open');
+})
+
+
