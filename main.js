30d6e3a41bd6dee4fb1dfeb41b614f1179a2a48e
RickFDND <rick.pijnacker@hva.nl>
2025-01-16 13:38:24 +0100

    hamburger menu af met kleine animatie close#9

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+//hamburger menu openen
 let hamburgerMenu = document.querySelector('.hamburger');
 let navbar = document.querySelector('nav');
 hamburgerMenu.addEventListener('click', function(){
     navbar.classList.toggle('hamburger-menu-open');
 })
 
-
+const logo = document.querySelector('header');
+    if (logo) {
+        window.addEventListener('scroll', () => {
+            logo.classList.toggle('scrolled', window.scrollY > 0);
+        });
+    }
